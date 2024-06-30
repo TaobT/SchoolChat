bcbf1e3f0cf22bc38effda98dd9d546079e7fcff
Ricardo7173 <riki0303@outlook.es>
2024-06-30 13:42:00 -0600

    chat basico

--- schoolchat_front/src/app/app-routing.module.ts
+++ schoolchat_front/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JoinServerComponent } from './componentes/join-server/join-server.component';
+import { ChatComponent } from "./componentes/chat/chat.component";
 
 const routes: Routes = [
-  { path: 'join-server', component: JoinServerComponent }
+  { 
+    path: 'join-server', 
+    component: JoinServerComponent 
+  },
+  { 
+    path: 'chat', 
+    component: ChatComponent 
+  },
+
 ];
 
 @NgModule({
--- schoolchat_front/src/app/app.module.ts
+++ schoolchat_front/src/app/app.module.ts
@@ -8,13 +8,16 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+//Componentes
 import { JoinServerComponent } from './componentes/join-server/join-server.component';
+import { ChatComponent } from './componentes/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     JoinServerComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
